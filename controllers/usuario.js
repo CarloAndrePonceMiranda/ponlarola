1c4383f0f025355637fa564e42798c1303639eb8
André Ponce <carloponce@MacBook-Pro-de-Andre.local>
2018-10-12 15:02:14 -0500

    Login, token de sesión y actualización de Usuario (backend) completos

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -6,6 +6,9 @@ var bcrypt = require('bcrypt-nodejs');
 // Importar modelo
 var User = require('../models/usuario');
 
+// Importar Servicio
+var jwt = require('../services/jwt');
+
 
 //Prueba
 function pruebas(req, res){
@@ -16,19 +19,19 @@ function pruebas(req, res){
 //Crear usuario
 function SaveUser(req, res) {
   var user = new User();
-  var parametros = req.body
+  var params = req.body
 
-  console.log(parametros);
+  console.log(params);
 
-  user.nombre = parametros.nombre;
-  user.apellido = parametros.apellido;
-  user.email = parametros.email;
+  user.nombre = params.nombre;
+  user.apellido = params.apellido;
+  user.email = params.email;
   user.role = 'ROLE_ADMIN';
   user.imagen = 'null';
 
-  if (parametros.password) {
+  if (params.password) {
     // Encriptar Contraseña
-    bcrypt.hash(parametros.password, null, null, function(err,hash){
+    bcrypt.hash(params.password, null, null, function(err,hash){
       user.password = hash;
       if (user.nombre != null && user.apellido != null && user.email != null) {
         //Guardar Usuario
@@ -42,26 +45,77 @@ function SaveUser(req, res) {
               res.status(404).send({mensaje:'Usuario no Registrado'});
             } else {
               // enviar error
-              res.status(200).send({usuario: userStored});
+              res.status(200).send({user: userStored});
             }
           }
         });
       }else {
         // enviar error
         res.status(200).send({mensaje:'Introduce Todos los Campos'});
-
       }
     });
-
   }else {
     // enviar error
     res.status(200).send({mensaje:'Introduce la contraseña...'});
   }
+}
+// Actualizar usuario
+function updateUser(req, res) {
+   var userId = req.params.id;
+   var update = req.body;
 
+   User.findByIdAndUpdate(userId, update, (err, userUpdated) => {
+     if (err) {
+       res.status(500).send({mensaje:'Error al Actualizar el usuario'});
+     } else {
+       if (!userUpdated) {
+         res.status(404).send({mensaje:'No se ah podido Actualizar el Usuario'});
+       }else {
+         res.status(200).send({user: userUpdated, mensaje:'Usuario Actualizado Correctamente'});
+       }
+     }
+   });
+}
+
+// Login
+function loginUser(req,res) {
+  var params = req.body;
+
+  var email = params.email;
+  var password = params.password;
+
+  User.findOne({email: email}, (err, user) =>  {
+    if (err) {
+      res.status(500).send({ mensaje : 'Error en la petición'});
+    } else {
+      if (!user) {
+        res.status(404).send({ mensaje : 'Usuario no registrado'});
+      } else {
+        // Comprobar Contraseña
+        bcrypt.compare(password, user.password, function(err,check) {
+          if (check) {
+            // Devolver los datos del Usuario Loggeado
+            if (params.gethash) {
+              // Devolver token jwt
+              res.status(200).send({
+                token: jwt.CreateToken(user)
+              });
+            }else {
+              res.status(200).send({ user });
+            }
+          } else {
+            res.status(404).send({ mensaje : 'Correo Electrónico o Contraseña incorrectos'});
+          }
+        });
+      }
+    }
+  });
 }
 
 // Exportar función
 module.exports = {
   pruebas,
-  SaveUser
+  SaveUser,
+  updateUser,
+  loginUser
 };
--- routes/usuario.js
+++ routes/usuario.js
@@ -7,9 +7,14 @@ var UserController = require('../controllers/usuario');
 // Cargar el Router de express
 var api = express.Router();
 
+// Cargar Middleware
+var md_auth = require('../middlewares/authenticated');
+
 // Crear Rutas
-api.get('/probando-controlador', UserController.pruebas)
+api.get('/probando-controlador', md_auth.ensureAuth, UserController.pruebas)
 api.post('/register', UserController.SaveUser)
+api.post('/login', UserController.loginUser)
+api.put('/update-user/:id', md_auth.ensureAuth, UserController.updateUser)
 
 // Exportar ruta
-module.exports = api ;
+module.exports = api;
