f552878d6a0fc4601ec3f09768ae3c06ba153aac
André Ponce <carloponce@MacBook-Pro-de-Andre.local>
2018-11-06 21:08:51 -0600

    Bug Fixes

--- app.js
+++ app.js
@@ -35,6 +35,8 @@ app.use((req,res,next)=>{
   res.header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
   res.header('Allow','GET, POST, PUT, DELETE, OPTIONS');
 
+  next();
+
 });
 
 // Rutas Base
