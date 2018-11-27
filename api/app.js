a5a9911d61118c4ad1195c7e47277485f641cdc1
André Ponce <carloponce@MacBook-Pro-de-Andre.local>
2018-11-26 18:44:48 -0600

    Sección 10: Login y registro en el FrontEnd, Terminado

--- api/app.js
+++ api/app.js
@@ -20,6 +20,7 @@ app.use((req,res,next)=>{
   res.header('Access-Control-Allow-Origin','*');
   /* ACCESO A TODAS LAS CABEZERAS */
   res.header('Access-Control-Allow-Headers','Authorization, X-API-KEY, ORIGIN, X-Requested-With, Content-Type, Accept', 'Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Headers','Content-Type');
   /* ACCESO A TODOS LOS METODOS HTTP */
   res.header('Access-Control-Allow-Methods','GET, POST, PUT, DELETE, OPTIONS');
   res.header('Allow','GET, POST, PUT, DELETE');
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,106 @@
-import { Component } from '@angular/core';
+import { Component, OnInit    } from '@angular/core';
 import { User } from './models/usuario';
+import { UserService } from './services/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers:[UserService],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   public title = '¡Pon La Rola!';
   public user : User;
+  public user_register : User;
   public identity;
   public token;
+  public errorMessage;
+  public alertRegister;
 
-  constructor(){
+  constructor(
+    private _userService: UserService,
+  ){
     this.user = new User('','','','','','ROLE_USER','');
+    this.user_register = new User('','','','','','ROLE_USER','');
   }
 
+  ngOnInit(){
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+  }
+
+  public onSubmit(){
+    // Consiguen Datos del Usuario
+    this._userService.signUp(this.user).subscribe(
+      response => {
+        let identity = response.user;
+        this.identity = identity;
+        if(!this.identity._id){
+          alert("No se inició sesión correctamente ❌");
+        }else{
+          //Crear elemento en el LocalStorage
+          localStorage.setItem('identity', JSON.stringify(identity));
+
+          //Conseguir el token para cada petición
+          this._userService.signUp(this.user, 'true').subscribe(
+            response => {
+              let token = response.token;
+              this.token = token;
+              if(this.token.length <= 0){
+                alert("El token no se Generó ❌");
+              }else{
+                //Crear elemento token en el LocalStorage
+                localStorage.setItem('token', token);
+                this.user = new User('','','','','','ROLE_USER','');
+              }
+            },
+            error => {
+              var errorMessage = <any>error;
+              if(errorMessage != null){
+                var body = JSON.parse(error._body);
+                this.errorMessage = body.mensaje;
+
+              }
+            }
+          );
+        }
+      },
+      error => {
+        var errorMessage = <any>error;
+        if(errorMessage != null){
+          var body = JSON.parse(error._body);
+          this.errorMessage = body.mensaje;
+        }
+      }
+    );
+  }
+
+
+  onSubmitRegister(){
+    this._userService.register(this.user_register).subscribe(
+      res => {
+        let user = res.user;
+        this.user_register = user;
+        if(!user._id){
+          this.alertRegister = 'Error al registrarse';
+        }else{
+          this.alertRegister = 'Registro exitoso, Inicia con '+this.user_register.email;
+          this.user_register = new User('','','','','','ROLE_USER','');
+        }
+      },
+      error => {
+        var errorMessage = <any>error;
+        if(errorMessage != null){
+          var body = JSON.parse(error._body);
+          this.alertRegister = body.mensaje;
+        }
+      }
+    );
+  }
+
+  logout(){
+    localStorage.removeItem('identity');
+    localStorage.removeItem('token');
+    localStorage.clear();
+    this.identity = null;
+    this.token = null;
+  }
 }
